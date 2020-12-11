7f2c8b6b905d37a01161ed361beebcbaaa947b78
Chara- <escor05_11@hotmail.com>
2020-12-11 04:21:52 -0500

    more config in bigcalendar

--- src/components/calendar/CalendarView.js
+++ src/components/calendar/CalendarView.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import Navbar from '../ui'
+import { messages } from '../../helpers/calendar-messages-es'
+import 'moment/locale/es'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+moment.locale('es')
 
 const localizer = momentLocalizer(moment)
 
@@ -12,22 +16,41 @@ const myEventList = [
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
     bgColor: '#fafafa',
+    notes: 'Comprar el pastel',
   },
 ]
 
-const CalendarView = () => (
-  <div>
-    <Navbar />
+const CalendarView = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log(event, start, end, isSelected)
 
-    <div style={{ display: 'flex', alignItems: 'center', height: '90vh' }}>
-      <BigCalendar
-        localizer={localizer}
-        events={myEventList}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 600, width: '100vw' }}
-      />
+    const style = {
+      backgroundColor: '#367cf7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: '#fff',
+    }
+
+    return { style }
+  }
+
+  return (
+    <div>
+      <Navbar />
+
+      <div style={{ display: 'flex', alignItems: 'center', height: '90vh' }}>
+        <BigCalendar
+          localizer={localizer}
+          events={myEventList}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 600, width: '100vw' }}
+          messages={messages}
+          eventPropGetter={eventStyleGetter}
+        />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 export default CalendarView
